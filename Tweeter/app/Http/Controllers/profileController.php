7ce4d9ca9d4e7d86cf88122d7b750908417e0c9f
jheslop25 <john@earthmountain.ca>
2020-02-04 20:29:53 +0000

    added user profiles. added update profile functions. user profile also doubles as a view for any profile but only shows edit options if user is profile owner

--- Tweeter/app/Http/Controllers/profileController.php
+++ Tweeter/app/Http/Controllers/profileController.php
@@ -10,15 +10,27 @@ class profileController extends Controller
     public function showUser($id){
         //shows user profile
         //get user details
-
+        $user = \App\User::where('id', $id)->get('name');
         //get all user tweets
-
+        //var_dump($user);
+        $tweets = \App\Tweets::where('user_id', $id)->get();
         //return view
-        return view();
+        return view('profile', ['tweets' => $tweets, 'user' => $user]);
     }
 
-    public function updateUser(){
+    public function updateUser(Request $request){
         //verify input, update user profile ?? this may be handled by the auth controllers
+        if($request->validate([
+            'name' => 'required | min:3 | max: 50',
+            'email' => 'required | email'
+        ])){
+            $id = Auth::user()->id;
+            $user = \App\User::find($id);
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->save();
+            return redirect('/user/' . Auth::user()->id);
+        }
     }
 
 }
