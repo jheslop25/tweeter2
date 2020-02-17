9a13c0584cbca8efb7d3c73ee779340c71bc70fa
jheslop25 <john@earthmountain.ca>
2020-02-17 00:13:37 +0000

    protected tweet comments against unauth users

--- Tweeter/app/Http/Controllers/tweetCommentsController.php
+++ Tweeter/app/Http/Controllers/tweetCommentsController.php
@@ -9,35 +9,38 @@ class tweetCommentsController extends Controller
 {
     public function createComment(Request $request){
         // verify input, AUTH, and passes values to model
-        if($request->validate([
-            'comment' => 'required | min:3 | max: 255',
-            'tweetId' => 'required'
-        ])){
-            $comment = new \App\Comments;
-            $comment->user_id = Auth::user()->id;
-            $comment->tweet_id = $request->tweetId;
-            $comment->content = $request->comment;
-            $comment->save();
-            return redirect("/tweets/view/$comment->tweet_id");
+        if(Auth::check()){
+            if($request->validate([
+                'comment' => 'required | min:3 | max: 255',
+                'tweetId' => 'required'
+            ])){
+                $comment = new \App\Comments;
+                $comment->user_id = Auth::user()->id;
+                $comment->tweet_id = $request->tweetId;
+                $comment->content = $request->comment;
+                $comment->save();
+                return redirect("/tweets/view/$comment->tweet_id");
+            }
+        } else {
+            return back();
         }
     }
 
     public function editComment(Request $request){
         // verify input, Auth and pass values to model
-        if($request->validate([
-            'comment' => 'required | min:3 | max:144',
-            'tweetId' => 'required',
-            'commentId' => 'required'
-        ])){
-            $id = $request->commentId;
-           $comment = \App\Comments::find($id);
-           $comment->content = $request->comment;
-           $comment->save();
-           return redirect("/tweets/view/$comment->tweet_id");
-        } else {
-            return redirect("/tweets/view/$comment->tweet_id");
-        }
-
+            if($request->validate([
+                'comment' => 'required | min:3 | max:144',
+                'tweetId' => 'required',
+                'commentId' => 'required'
+            ])){
+                $id = $request->commentId;
+               $comment = \App\Comments::find($id);
+               $comment->content = $request->comment;
+               $comment->save();
+               return redirect("/tweets/view/$comment->tweet_id");
+            } else {
+                return redirect("/tweets/view/$comment->tweet_id");
+            }
     }
 
     public function deleteComment(Request $request){
--- Tweeter/resources/views/comments/create.blade.php
+++ Tweeter/resources/views/comments/create.blade.php
@@ -1,5 +1,6 @@
 <form class="card p-3" action="/tweets/comment/create" method="post">
     @csrf
+    <input type="text" name="user_id" value="{{$tweet->user_id}}" class="d-none" readonly>
     <input class="form-control text-muted" type="text" name="comment" value="Make a comment">
     <input type="number" name="tweetId" value="{{$tweet->id}}" readonly class="d-none">
     <button class="btn btn-primary mt-2" type="submit">Comment</button>
