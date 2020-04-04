a76ac96400e307ac4e121d2a9126fc77ccec9b25
jheslop25 <john@earthmountain.ca>
2020-04-04 20:42:39 +0000

    added file

---
+++ Tweeter/resources/js/components/TweetFeed.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <Tweet
+            v-for="tweet in tweets"
+            v-bind:key="tweet"
+        />
+    </div>
+</template>
+
+<script>
+    import Tweet from './Tweet.vue';
+
+    export default {
+        name: 'TweetFeed',
+        methods: {
+
+        },
+        mounted: {
+
+        },
+        data(){
+            return {
+                tweets: []
+            }
+        },
+        components: {
+
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
